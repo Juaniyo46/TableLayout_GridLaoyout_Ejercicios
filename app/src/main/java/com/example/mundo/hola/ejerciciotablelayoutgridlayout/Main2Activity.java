ee933c48a09d4e3e0150d49d28b4be0c32c45ed8
Juaniyo46 <juan.alvarez@escuelaestech.es>
2019-02-08 17:06:43 +0100

    Terminado

--- app/src/main/java/com/example/mundo/hola/ejerciciotablelayoutgridlayout/Main2Activity.java
+++ app/src/main/java/com/example/mundo/hola/ejerciciotablelayoutgridlayout/Main2Activity.java
@@ -1,7 +1,10 @@
 package com.example.mundo.hola.ejerciciotablelayoutgridlayout;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        final Button button = findViewById(R.id.atras);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Main2Activity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
